53f165c45bded494240dd0bfab90977c82259173
steerm <Matthew_Steer@BATHNES.GOV.UK>
2017-02-02 09:24:24 +0000

    Additional Files

---
+++ tmp/config.constants.js
@@ -0,0 +1,16 @@
+angular.module("BB")
+
+.constant("bbConfig", {
+    "BUILD": {
+        "APP_NAME": "WasteManagement_BathCentral",
+        "DEFAULT_HTML": "/new_booking.html",
+        "SERVER_PORT": 8000,
+        "SDK_VERSION": "2.0.44"
+    },
+    "CORE": {
+        "API_URL": "https://banes.council.bookingbug.com",
+        "COMPANY_ID": "37025"
+    }
+})
+
+;
